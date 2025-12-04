fd12b9a6f9da117c0aa8d02f8800dfcb231eef75
Jeevitha-Prakash <jeevitha.prakash2014@gmail.com>
2025-12-04 13:31:27 +0530

    4-12-2025

---
+++ src/Excercise_3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Excercise_3 {
+
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int rows = sc.nextInt();
+        System.out.println("Enter the number of coloumns");
+        int coloumns = sc.nextInt();
+        for(int i=0; i<rows;i++)
+        {
+            for (int j = rows - 1; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
